5fe122e02b2cb7927cacd349dd68314fb63ad1cc
Brhando <brandonsisco21@gmail.com>
2026-01-28 06:12:18 -0700

    Added User class

---
+++ pond_backend/src/main/java/com/brhando/pond/pond_backend/domain/User.java
@@ -0,0 +1,34 @@
+package com.brhando.pond.pond_backend.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    @Column(unique = true) // needs to be unique, as it's used for login
+    private String email;
+
+    // constructors
+    protected User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // getters/setters
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+}
---
+++ pond_backend/src/test/java/com/brhando/pond/pond_backend/PondBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brhando.pond.pond_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PondBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
